84df4f19ee3c70296a09d5c2f53eeedd1424fc3e
owen <1297113406@qq.com>
2023-09-22 00:34:42 +0800

    添加前端vue框架, docker-compose文件

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # video_site
+**练习项目，主要为熟悉前后端开发，数据库使用，容器化部署**
+
+## 前端
+位于frontend文件夹下  
+Node: v18  
+Vuejs: 3  
+Ant Vue
+
+## 后端
+位于backend文件夹下  
+Django 4  
+
+## 数据库
+容器化部署  
+Postgres 13
+Redis
+
+## 容器化
+Docker
---
+++ frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <h1>Hellsaoaaaaaaaaaaaaa</h1>
+  <video controls>
+      <source src="../public/BigCockBully-India_Summer.mp4" type="video/mp4">
+  </video>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
