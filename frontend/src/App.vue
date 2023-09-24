e04ddebb7b5116d85d28b3cef7eba7f54228cae6
Zenbook-virtual <1297113406@qq.com>
2023-09-24 21:17:05 +0800

    前端框架建立

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,47 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <h1>Hellsaoaaaaaaaaaaaaa</h1>
-  <video controls>
-      <source src="../public/BigCockBully-India_Summer.mp4" type="video/mp4">
-  </video>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import { theme } from 'ant-design-vue'
 
 export default {
   name: 'App',
   components: {
-    // HelloWorld
+  },
+  data(){
+    return({
+      theme
+    })
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+
+#components-layout-demo-fixed .logo {
+  width: 120px;
+  height: 31px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px 24px 16px 0;
+  float: left;
 }
+.site-layout .site-layout-background {
+  background: #fff;
+}
+
+[data-theme='dark'] .site-layout .site-layout-background {
+  background: #141414;
+}
+
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,26 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { 
+    Button,
+    ConfigProvider,
+    Card,
+    Layout,
+    Menu,
+    Breadcrumb
+} from 'ant-design-vue';
+import router from './router'
+import App from './App';
+import 'ant-design-vue/dist/reset.css'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(router)
+
+// antd-vue
+app.use(Button)
+app.use(ConfigProvider)
+app.use(Card)
+app.use(Layout)
+app.use(Menu)
+app.use(Breadcrumb)
+
+app.mount('#app');
